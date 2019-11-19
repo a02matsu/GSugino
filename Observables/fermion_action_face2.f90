445b033052451f0c2ddd63bfdd2e7cfbacaeda6d
So Matsuura <s.matsu@phys-h.keio.ac.jp>
2019-11-19 21:40:34 +0900

    update calcobs

--- Observables/fermion_action_face2.f90
+++ Observables/fermion_action_face2.f90
@@ -21,7 +21,7 @@ do lf=1,num_faces
     call fermionic_face_lagrangian(Lff,lf,l_place,Glambda_chi,Umat)
     dir = links_in_f(lf)%link_dirs_(l_place) 
     tmp_Sf_face = tmp_Sf_face &
-      + dcmplx( dble(dir) * alpha_f(lf)*beta_f(lf) ) * Lff
+      - dcmplx( dble(dir) * alpha_f(lf)*beta_f(lf) ) * Lff
   enddo
 enddo
 
