d704b4322cfeed878d61e6f34a3d0f4e235be2de
So Matsuura <s.matsu@phys-h.keio.ac.jp>
2019-11-19 21:36:11 +0900

    update calcobs

--- calcobs.f90
+++ calcobs.f90
@@ -237,6 +237,7 @@ do
     !! trivial Sf_face2
       call calc_Sf_face2(Sf,Glambda_chi,Umat)
       if( MYRANK == 0 ) write(*,'(E15.8,2X)',advance='no')  dble(Sf)
+      if( MYRANK == 0 ) write(*,'(E15.8,2X)',advance='no')  dble((0d0,1d0)*Sf)
 
     !! mass contribution in WT_site
       call mass_contribution_site(mass_cont,Geta_eta,PhiMat)
@@ -249,7 +250,6 @@ do
     !! mass contribution in WT_face
       call mass_contribution_face(mass_cont,Gchi_eta,Umat,PhiMat)
        if( MYRANK == 0 ) write(*,'(E15.8,2X)',advance='no')  dble(mass_cont)
-       if( MYRANK == 0 ) write(*,'(E15.8,2X)',advance='no')  dble((0d0,1d0)*mass_cont)
 
 
 
