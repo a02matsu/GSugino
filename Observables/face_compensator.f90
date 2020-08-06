c13011620f4e1d4ce128d3bb48a0f2c58452f1f6
So Matsuura <s.matsu@phys-h.keio.ac.jp>
2020-08-06 21:47:56 +0900

    face compensator for face

--- Observables/face_compensator.f90
+++ Observables/face_compensator.f90
@@ -559,7 +559,7 @@ do gf=1,global_num_faces
               tmp2=tmp2 + Phimat(a,b,ls)*Feta(b,a,ls,j,i,ratio-p-1,gf)
             enddo
           enddo
-          write(*,*) gf,p,i,j, MYRANK
+          write(*,*) gf,p, MYRANK
           call MPI_REDUCE(tmp1,trace1,1,MPI_DOUBLE_COMPLEX, &
             MPI_SUM,0,MPI_COMM_WORLD,IERR)
           call MPI_REDUCE(tmp2,trace2,1,MPI_DOUBLE_COMPLEX, &
