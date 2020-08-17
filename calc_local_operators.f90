1cb9c5e613431f75d5f6d3e002b8368c6c80154e
So Matsuura <s.matsu@phys-h.keio.ac.jp>
2020-08-17 14:49:05 +0900

    local operators

--- calc_local_operators.f90
+++ calc_local_operators.f90
@@ -3,7 +3,7 @@ program main
 use global_parameters
 use initialization_calcobs
 use simulation
-use matrix_functions, only : trace_mm
+use matrix_functions, only : trace_mm, hermitian_conjugate, matrix_power, matrix_inverse
 use parallel
 implicit none
 
