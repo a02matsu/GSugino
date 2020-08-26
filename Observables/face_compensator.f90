303ec7fdd5c5b0cfac94366e6b56b5dbdc94f6a3
So Matsuura <s.matsu@phys-h.keio.ac.jp>
2020-08-26 17:15:05 +0900

    regularized_compensator is introduced. face_compensator is replaced.

--- Observables/face_compensator.f90
+++ Observables/face_compensator.f90
@@ -720,12 +720,12 @@ contains
   end subroutine make_phibar_p
   
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  subroutine syncronize_sites_large(eta,ratio)
+  subroutine syncronize_sites_large(eta,power)
   use parallel
-  complex(kind(0d0)), intent(inout) :: eta(1:NMAT,1:NMAT,1:num_necessary_sites,1:NMAT,1:NMAT,0:ratio-1,1:global_num_faces) 
-    integer, intent(in) :: ratio
+  complex(kind(0d0)), intent(inout) :: eta(1:NMAT,1:NMAT,1:num_necessary_sites,1:NMAT,1:NMAT,0:power-1,1:global_num_faces) 
+  integer, intent(in) :: power
   
-  complex(kind(0d0)) :: tmp_eta(1:NMAT,1:NMAT,1:NMAT,1:NMAT,0:ratio-1,1:global_num_faces,num_necessary_sites)
+  complex(kind(0d0)) :: tmp_eta(1:NMAT,1:NMAT,1:NMAT,1:NMAT,0:power-1,1:global_num_faces,num_necessary_sites)
   integer :: s_send
   integer :: s_recv
   integer :: local, rank, tag
@@ -733,12 +733,12 @@ contains
   
   integer :: ls, gf, r, i,j, num
   
-  num=NMAT**4*ratio*global_num_faces
+  num=NMAT**4*power*global_num_faces
   !! prepare tmp_lambda
   tmp_eta=(0d0,0d0)
   do ls=1,num_sites
     do gf=1,global_num_faces
-      do r=0, ratio-1
+      do r=0, power-1
         do j=1,NMAT
           do i=1,NMAT
             tmp_eta(:,:,i,j,r,gf,ls)=eta(:,:,ls,i,j,r,gf)
@@ -778,7 +778,7 @@ contains
   !! from tmp_eta to eta
   do ls=1,num_necessary_eta
     do gf=1,global_num_faces
-      do r=0, ratio-1
+      do r=0, power-1
         do j=1,NMAT
           do i=1,NMAT
             eta(:,:,ls,i,j,r,gf)=tmp_eta(:,:,i,j,r,gf,ls)
@@ -792,13 +792,13 @@ contains
   
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  subroutine syncronize_links_large(lambda,ratio)
+  subroutine syncronize_links_large(lambda,power)
   use global_parameters
   use parallel
-  complex(kind(0d0)), intent(inout) :: lambda(1:NMAT,1:NMAT,1:num_necessary_links,1:NMAT,1:NMAT,0:ratio-1,1:global_num_faces) 
-  integer, intent(in) :: ratio
+  complex(kind(0d0)), intent(inout) :: lambda(1:NMAT,1:NMAT,1:num_necessary_links,1:NMAT,1:NMAT,0:power-1,1:global_num_faces) 
+  integer, intent(in) :: power
   
-  complex(kind(0d0)) :: tmp_lambda(1:NMAT,1:NMAT,1:NMAT,1:NMAT,0:ratio-1,1:global_num_faces, 1:num_necessary_links)
+  complex(kind(0d0)) :: tmp_lambda(1:NMAT,1:NMAT,1:NMAT,1:NMAT,0:power-1,1:global_num_faces, 1:num_necessary_links)
   integer :: s_send
   integer :: s_recv
   integer :: local, rank, tag
@@ -807,12 +807,12 @@ contains
   
   integer :: ll, gf, r, i,j, num
   
-  num=NMAT**4*ratio*global_num_faces
+  num=NMAT**4*power*global_num_faces
   !! prepare tmp_lambda
   tmp_lambda=(0d0,0d0)
   do ll=1,num_links
     do gf=1,global_num_faces
-      do r=0, ratio-1
+      do r=0, power-1
         do j=1,NMAT
           do i=1,NMAT
             tmp_lambda(:,:,i,j,r,gf,ll)=lambda(:,:,ll,i,j,r,gf)
@@ -853,7 +853,7 @@ contains
   !! from tmp_lambda to tmp
   do ll=1,num_necessary_links
     do gf=1,global_num_faces
-      do r=0, ratio-1
+      do r=0, power-1
         do j=1,NMAT
           do i=1,NMAT
             lambda(:,:,ll,i,j,r,gf)=tmp_lambda(:,:,i,j,r,gf,ll)
@@ -865,13 +865,13 @@ contains
   end subroutine syncronize_links_large
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  subroutine syncronize_faces_large(chi,ratio)
+  subroutine syncronize_faces_large(chi,power)
   use global_parameters
   use parallel
-  complex(kind(0d0)), intent(inout) :: chi(1:NMAT,1:NMAT,1:num_necessary_faces,1:NMAT,1:NMAT,0:ratio-1,1:global_num_faces) 
-  integer, intent(in) :: ratio
+  complex(kind(0d0)), intent(inout) :: chi(1:NMAT,1:NMAT,1:num_necessary_faces,1:NMAT,1:NMAT,0:power-1,1:global_num_faces) 
+  integer, intent(in) :: power
   
-  complex(kind(0d0)) :: tmp_chi(1:NMAT,1:NMAT,1:NMAT,1:NMAT,0:ratio-1,1:global_num_faces, 1:num_necessary_faces)
+  complex(kind(0d0)) :: tmp_chi(1:NMAT,1:NMAT,1:NMAT,1:NMAT,0:power-1,1:global_num_faces, 1:num_necessary_faces)
   integer :: s_send
   integer :: s_recv
   integer :: local, rank, tag
@@ -880,12 +880,12 @@ contains
   
   integer :: lf, gf, r, i,j, num
   
-  num=NMAT**4*ratio*global_num_faces
+  num=NMAT**4*power*global_num_faces
   !! prepare tmp_chi
   tmp_chi=(0d0,0d0)
   do lf=1,num_faces
     do gf=1,global_num_faces
-      do r=0, ratio-1
+      do r=0, power-1
         do j=1,NMAT
           do i=1,NMAT
             tmp_chi(:,:,i,j,r,gf,lf)=chi(:,:,lf,i,j,r,gf)
@@ -926,7 +926,7 @@ contains
   !! from tmp_chi to tmp
   do lf=1,num_necessary_faces
     do gf=1,global_num_faces
-      do r=0, ratio-1
+      do r=0, power-1
         do j=1,NMAT
           do i=1,NMAT
             chi(:,:,lf,i,j,r,gf)=tmp_chi(:,:,i,j,r,gf,lf)
