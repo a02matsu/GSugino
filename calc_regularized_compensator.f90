e8fc3ade5843ad1cc811589ab3e104eff9dc9a74
So Matsuura <s.matsu@phys-h.keio.ac.jp>
2020-09-04 11:02:37 +0900

    regularized compensator ver.2 is introduced

--- calc_regularized_compensator.f90
+++ calc_regularized_compensator.f90
@@ -1,4 +1,8 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!! use as
+!! % ./a.out [MEDFILE] [Ckind]
+!!   Ckind=1 : A = sum_s ( Tr( (\phi + \epsilon)^2 )^{-r/4} )
+!!   Ckind=2 : A = 1 / sum_s (Tr(\phi^2))^r/4 + \epsilon)
 program main
 use global_parameters
 use initialization_calcobs
@@ -15,6 +19,8 @@ integer, parameter :: num_operators=4
 
 integer :: control
 character(128) :: MEDFILE
+character(10) :: C_Ckind
+integer :: Ckind
 integer, parameter :: N_MEDFILE=100
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -25,7 +31,7 @@ integer :: num_fermion, eular, ratio
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !! misc
 double precision, parameter :: max_epsilon=5.0d0
-double precision, parameter :: interval=0.1d0
+double precision, parameter :: interval=0.01d0
 double precision :: regulator
 integer :: ite
 integer :: i
@@ -34,10 +40,21 @@ integer :: i
 !! initialization
 iarg=iargc()
 if( iarg < 1 ) then
-  if (MYRANK==0) write(*,*) "use as a.out [MEDFILE]"
+  if (MYRANK==0) write(*,*) "use as a.out [MEDFILE] [kind of compensator]"
   stop
 endif
 call getarg(1,MEDFILE)
+if( iarg >= 2 ) then
+  call getarg(2,C_Ckind)
+  read(C_Ckind,*) Ckind
+else
+  Ckind=1
+endif
+if( Ckind >= 3 .or. Ckind <= 0 ) then
+  if (MYRANK==0) write(*,*) "Ckind = 1 or 2 at present."
+  stop
+endif
+
 INPUT_FILE_NAME="inputfile"
 
 call initialization 
@@ -55,7 +72,12 @@ endif
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !! output measurements 
 if( MYRANK == 0 ) then
-  write(*,'(a,f6.2,a,f6.2)') "# min=0.0, max=",max_epsilon,", interval=",interval
+  write(*,'(a,f6.2,a,f6.2)', advance='no') "# min=0.0, max=",max_epsilon,", interval=",interval
+  if( Ckind == 1 ) then
+    write(*,'(a)') "A = sum_s ( Tr( (\phi + \epsilon)^2 )^{-r/4} )"
+  elseif( Ckind == 2 ) then 
+    write(*,'(a)') "A = 1 / sum_s (Tr(\phi^2))^r/4 + \epsilon)"
+  endif
 endif
 do 
   !! read configuration
@@ -69,6 +91,7 @@ do
 
   !"|Atr|", &
   call calc_trace_compensator(Acomp,PhiMat)
+
   if( MYRANK == 0 ) write(*,'(E15.8,2X)',advance='no') cdabs(Acomp)
   if( MYRANK == 0 ) write(*,'(E15.8,2X)',advance='no') dble(Acomp/cdabs(Acomp))
   if( MYRANK == 0 ) write(*,'(E15.8,2X)',advance='no') dble( (0d0,-1d0)*Acomp/cdabs(Acomp))
@@ -77,7 +100,11 @@ do
   !!!!!!!!!!!!!!!!
   regulator=interval 
   do while ( regulator < max_epsilon + interval )
-    call calc_regularized_compensator(Acomp_reg,PhiMat,regulator)
+    if( Ckind == 1 ) then
+      call calc_regularized_compensator(Acomp_reg,PhiMat,regulator)
+    elseif( Ckind == 2 ) then
+      call calc_regularized_compensator_v2(Acomp_reg,PhiMat,regulator)
+    endif
     if( MYRANK == 0 ) write(*,'(E15.8,2X)',advance='no') cdabs(Acomp_reg)
     if( MYRANK == 0 ) write(*,'(E15.8,2X)',advance='no') dble(Acomp_reg/cdabs(Acomp_reg))
     if( MYRANK == 0 ) write(*,'(E15.8,2X)',advance='no') dble( (0d0,-1d0)*Acomp_reg/cdabs(Acomp_reg))
